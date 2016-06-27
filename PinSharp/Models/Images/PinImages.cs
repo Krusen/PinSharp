0d3e96080e18760032b202cc6d6ae412d6af32a1
Søren Kruse <krusenen@gmail.com>
2016-06-27 19:28:47 +0200

    Updated image lists to match what is returned from the API

--- PinSharp/Models/Images/PinImages.cs
+++ PinSharp/Models/Images/PinImages.cs
@@ -4,15 +4,6 @@ namespace PinSharp.Models.Images
 {
     public class PinImages
     {
-        [JsonProperty("70x")]
-        public ImageInfo W70 { get; set; }
-
-        [JsonProperty("236x")]
-        public ImageInfo W236 { get; set; }
-
-        [JsonProperty("736x")]
-        public ImageInfo W736 { get; set; }
-
         public ImageInfo Original { get; set; }
     }
 }
--- PinSharp/Models/Images/UserImages.cs
+++ PinSharp/Models/Images/UserImages.cs
@@ -4,16 +4,7 @@ namespace PinSharp.Models.Images
 {
     public class UserImages
     {
-        [JsonProperty("60x")]
+        [JsonProperty("60x60")]
         public ImageInfo W60 { get; set; }
-
-        [JsonProperty("110x")]
-        public ImageInfo W110 { get; set; }
-
-        [JsonProperty("165x")]
-        public ImageInfo W165 { get; set; }
-
-        [JsonProperty("280x")]
-        public ImageInfo W280 { get; set; }
     }
 }
