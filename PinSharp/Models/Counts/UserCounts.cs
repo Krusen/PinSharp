0f89c8bdf1e7b563ea7f3c7f8cd80605548f9265
Søren Kruse <krusenen@gmail.com>
2016-06-27 19:28:13 +0200

    Removed Collaborators count from users

--- PinSharp/Models/Counts/UserCounts.cs
+++ PinSharp/Models/Counts/UserCounts.cs
@@ -4,7 +4,6 @@
     {
         public int Followers { get; set; }
         public int Pins { get; set; }
-        public int Collaborators { get; set; }
         public int Following { get; set; }
         public int Boards { get; set; }
         public int Likes { get; set; }
